bf654622f069c285ad8a768e5d04c506feb79433
Ricardo Espirito Santo <ricardo.santo@evrythng.com>
2019-05-11 20:25:52 +0100

    Initial push

---
+++ src/main/java/com/ricardoespsanto/opsgenie2Harvest/Opsgenie2HarvestApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoespsanto.opsgenie2Harvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Opsgenie2HarvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Opsgenie2HarvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardoespsanto/opsgenie2Harvest/Opsgenie2HarvestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ricardoespsanto.opsgenie2Harvest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Opsgenie2HarvestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
